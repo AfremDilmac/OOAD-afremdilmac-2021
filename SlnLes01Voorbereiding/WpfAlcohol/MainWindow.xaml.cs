6e12be91cd2ad508b22a0840718ee6a0782f2567
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2021-02-09 18:43:09 +0100

    commit

---
+++ SlnLes01Voorbereiding/WpfAlcohol/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAlcohol
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sldBier_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblBier.Content = Convert.ToString(Math.Round(sldBier.Value) + " glazen");
+            rtlBalk.Width = Convert.ToInt32(sldBier.Value);
+        }
+
+        private void sldWijn_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblWijn.Content = Convert.ToString(Math.Round(sldWijn.Value) + " glazen");
+            rtlBalk.Width = Convert.ToInt32(sldWijn.Value);
+        }
+
+        private void sldWhisky_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblWhisky.Content = Convert.ToString(Math.Round(sldWhisky.Value) + " glazen");
+            rtlBalk.Width += Convert.ToInt32(sldWhisky.Value);
+        }
+    }
+}
