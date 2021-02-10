22b1c655c1d188d25e7cc41d60a6448250a409f0
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2021-02-10 13:16:47 +0100

    commit

--- SlnLes01Voorbereiding/ConsoleBankautomaat/Program.cs
+++ SlnLes01Voorbereiding/ConsoleBankautomaat/Program.cs
@@ -30,10 +30,7 @@ namespace ConsoleBankautomaat
                 }
             } while (pincode != 1111);
 
-         
-
-
-
+        
             //Operaties binnen een do while
             //u kunt altijd kiezen tot dat u c kiest om te stoppen
             do
@@ -46,27 +43,22 @@ namespace ConsoleBankautomaat
                 //Hier kan men een bedrag afhalen
                 if (keuze == "a"  )
                 {
-
                     Console.WriteLine("Welk bedrag wil je afhalen: ");
                     double afhalen = Convert.ToDouble(Console.ReadLine());
                     nieuwesaldo = Convert.ToDouble(saldo - afhalen);
                     if (nieuwesaldo < 0)
                     {
                         Console.WriteLine("Fout! u kunt niet onder de saldo gaan");
-
                     }
                     else
                     {
                         saldo = nieuwesaldo;
                         Console.WriteLine($"Afhaling ok - het nieuwe saldo is {saldo}");
-
                     }
-                 
                 }
                 //Hier kan men een bedrag instorten
                 else if (keuze == "b")
                 {
- 
                     Console.WriteLine("Welk bedrag wil je storten: ");
                     double storten = Convert.ToDouble(Console.ReadLine());
                     nieuwesaldo = Convert.ToDouble(saldo + storten);
