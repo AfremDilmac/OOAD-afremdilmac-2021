a08bf57ecb9904a171456f170f878bdb8cad5f37
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2021-02-17 23:03:41 +0100

    commit

---
+++ SlnLes02/ConsoleKarakter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleKarakter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaratie 
+            string invoer;
+            Console.Write("Geef een letter: ");
+            invoer = Convert.ToString(Console.ReadLine());
+
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ SlnLes02/ConsoleKlinkers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleKlinkers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string naam;
+            int aantalKlinkers = 0;
+            int aantalSpaties = 0;
+            Console.Write("Geef een tekst: ");
+            naam = Convert.ToString(Console.ReadLine());
+
+            for (int i = 0; i < naam.Length; i++)
+            {
+                if (naam[i] == 'A' || naam[i] == 'E' || naam[i] == 'I' || naam[i] == 'O' || naam[i] == 'U' || naam[i] == 'a' || naam[i] == 'e' || naam[i] == 'i' || naam[i] == 'o' || naam[i] == 'u')
+                {
+                    aantalKlinkers++;
+                }
+            }
+           
+            char letter = Convert.ToChar(naam);
+            letter++;
+            string naamVerandering = naam;
+
+
+
+            Console.WriteLine($"deze tekst bevat {aantalKlinkers} klinkers");
+            Console.WriteLine($"{naamVerandering}");
+            Console.ReadKey();
+        }
+    }
+}
