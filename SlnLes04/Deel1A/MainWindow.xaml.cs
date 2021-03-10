1e5adb4a764e3b4f02efe0a4e17521c32a20b0d1
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2021-03-10 13:03:48 +0100

    commit

---
+++ SlnLes04/Deel1A/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Deel1A
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void exitItem_Click(object sender, RoutedEventArgs e)
+        {
+            // 0 wilt zeggen dat er niets is fout gelopen
+            Environment.Exit(0);
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.Filter = "Tekstbestanden|*.TXT;*.TEXT";
+            string chosenFileName;
+            if (dialog.ShowDialog() == true)
+            {
+                // user picked a file and pressed OK
+                chosenFileName = dialog.FileName;
+                tbxContent.Text = File.ReadAllText(dialog.FileName);
+                FileInfo fi = new FileInfo(chosenFileName);
+                lblStatus.Content = $"#chars: {fi.Length}";
+            }
+            else
+            {
+                // user cancelled or escaped dialog window
+            }
+
+        }
+    }
+}
